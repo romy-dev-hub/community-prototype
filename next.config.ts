6bd292807d7b64d740e79eac457339db4bac8b9f
xiao <roumaissa.hadibi.dev@gmail.com>
2025-08-23 14:22:30 +0100

    /

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
    eslint: {
     ignoreDuringBuilds: true,
   },
